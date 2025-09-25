e987028244c552d50b4f4be52c733a1dd69db958
Orlando G.V <orlando.gal.var05@gmail.com>
2025-09-25 17:31:17 -0600

    Prueba GIt error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         GitHub Actions
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
