87f708b0f2add5ffdbfa87ab9216e300eb8150ab
Orlando G.V <orlando.gal.var05@gmail.com>
2025-09-25 15:44:35 -0600

    Prueba correcta 1

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hola Mundo
+        GitHub Actions
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
